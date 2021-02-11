d2b95166eaf949c34d51b8bdf8d04427d939eb05
Ygor Ferreira <ygor.pinto@hotmail.com>
2021-02-11 01:03:43 -0300

    ok

--- components/Title.js
+++ components/Title.js
@@ -3,7 +3,7 @@ import styled from 'styled-components'
 const Title = styled.h1`
 font-size:8vh;
 font-family:'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
-text-shadow:3px 3px rgba(0,0,0,0.5);
+text-shadow:2px 2px rgba(0,0,0,0.5);
 @media screen and (max-width:450px){
     font-size:5vh;
     }
